9621ca9783edab64ce153e03b0bb60df913fc90d
utakeuchinojabaibai <183874927+utakeuchinojabaibai@users.noreply.github.com>
2025-06-16 08:57:36 +0900

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,52 @@
+// sw.js
+
+const CACHE_NAME = 'my-site-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/script.js'
+];
+
+// インストールイベント
+self.addEventListener('install', (event) => {
+  // キャッシュの準備
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// アクティブ化イベント
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// フェッチイベント
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // キャッシュから取得
+        if (response) {
+          return response;
+        }
+        // キャッシュにない場合はネットワークから取得
+        return fetch(event.request);
+      })
+  );
+});
