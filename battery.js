16e48908f7133bcc0de2f1a474fec442b2949f62
utakeuchinojabaibai <183874927+utakeuchinojabaibai@users.noreply.github.com>
2025-07-11 11:51:25 +0900

    Update battery.js

--- battery.js
+++ battery.js
@@ -1,43 +1,18 @@
-(function(Scratch) {
-    'use strict';
-
-    // 拡張機能の名前
-    const EXTENSION_NAME = 'MyCustomExtension';
-    // 拡張機能のID
-    const EXTENSION_ID = 'myCustomExtension';
-
-    // ブロックの情報を定義
-    const blocks = [
-        {
-            opcode: 'myCustomBlock',
-            blockType: Scratch.BlockType.COMMAND,
-            text: 'カスタムブロックを実行する',
-            arguments: {},
-        },
-    ];
-
-    // 拡張機能の情報を定義
-    const info = {
-        id: EXTENSION_ID,
-        name: EXTENSION_NAME,
-        blocks: reporter,
-    };
-
-    // 拡張機能のクラスを定義
-    class MyCustomExtension {
-        getInfo() {
-            return info;
-        }
-
-        myCustomBlock() {
+myCustomBlock() {
            navigator.getBattery().then(function(battery) {
 var battery = battery.charging;
 }); 
 return battery;
         }
-    }
-
-    // 拡張機能を登録
-    Scratch.extensions.register(new MyCustomExtension());
-
-})(Scratch);
+window.ScratchExtensions.register('Weather Info Extension', {
+    blocks: [
+        {
+            opcode: 'myCustomBlock',
+            blockType: 'reporter',
+            text: 'battery',
+        },
+    ],
+    execute() {
+        return getWeather();
+    },
+});
